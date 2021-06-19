5456e7aa4fb63b2d9eb2df050bad7337453d2251
hannaleve <wbtmgkssk@naver.com>
2021-06-19 21:34:07 +0900

    .

---
+++ Test.java
@@ -0,0 +1,27 @@
+package com.eomcs.lang.test;
+
+public class Test {
+
+  public static int countEventNumber(int value) {
+    int result = 0;
+    int n = value;
+
+    while(n!=0) {
+      if((n%2)==0) {
+        result++;
+      }
+      n/=10;
+    }
+    return result;
+  }
+  public static void main(String[] args) {
+
+    //주어진 숫자 중 짝수의 개수세기
+
+    System.out.println(countEventNumber(1238694636)); //6
+
+    //286466 = 6
+
+  }
+
+}
